887497b414ce3008b02be175a7d2cd12feef8200
Shabex <shabsibrah@gmail.com>
2022-06-27 15:28:09 +0300

    commit tree e6c798bbf56686b1c885f4f5acb58be11d2ec840

---
+++ src/00-objectball.js
@@ -0,0 +1,12 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "w"},
+    {year: "2016", result: "N/A"}
+]
+
+function superBowlWin(teamData) {
+
+    console.log(teamData)
+}
+
+superBowlWin(record)
